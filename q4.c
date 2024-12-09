db383534a6b8cb64a07e19bba006e25d60c6dc7f
dev-on-board <bhabanisuar123@gmail.com>
2024-12-10 00:03:48 +0530

    update

---
+++ q4.c
@@ -0,0 +1,11 @@
+//WAP to find the area of the circle. Take radius of circle from user as input and print the
+//result in below given format.
+
+#include<stdio.h>
+int main(){
+   float r;
+   printf("enter the radius : ");
+   scanf("%f",&r);
+   printf("\narea is %.2f",3.14*r*r);
+   return 0;
+}
---
+++ q5.c
@@ -0,0 +1,8 @@
+//WAP to calculate the length of String using printf function
+
+#include<stdio.h>
+int main(){
+   int a =printf("dev bhabani sankar");
+   printf("\nlength of the word is %d",a);
+   return 0;
+}
